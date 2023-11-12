1b652df0e97e111f7f59c04c3e5b889064b73da3
ranayajr <ramonanaya6905@gmail.com>
2023-11-11 16:26:03 -0800

    index.html

--- stuff.js
+++ stuff.js
@@ -1,12 +1,12 @@
 function getValue() {
     // Get the value from the input field
-  var from = document.getElementById("from").value;
-  var to = document.getElementById("to").value;
+  var location = document.getElementById("location").value;
+  var destination = document.getElementById("destination").value;
   var mileage = document.getElementById("mileage").value;
 
     // Now 'userInput' contains the value entered by the user
     // You can perform actions or logic with this variable
-  console.log("From: ", from);
-  console.log("To: ", to);
-  console.log("Mileage:", mileage);
+  console.log("location: ", location);
+  console.log("destination: ", destination);
+  console.log("mileage:", mileage);
 }
